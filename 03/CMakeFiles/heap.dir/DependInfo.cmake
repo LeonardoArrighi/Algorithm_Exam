18aa609862058b69482a66805374cd2e1ef148ea
Leonardo <larrighi@sissa.it>
2020-08-20 00:09:19 +0200

    HW03 almost finished

---
+++ 03/CMakeFiles/heap.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/leonardo/DSSC/Algorithms/Homeworks/Algorithm_Exam/03/src/binheap.c" "/home/leonardo/DSSC/Algorithms/Homeworks/Algorithm_Exam/03/CMakeFiles/heap.dir/src/binheap.c.o"
+  "/home/leonardo/DSSC/Algorithms/Homeworks/Algorithm_Exam/03/src/main.c" "/home/leonardo/DSSC/Algorithms/Homeworks/Algorithm_Exam/03/CMakeFiles/heap.dir/src/main.c.o"
+  "/home/leonardo/DSSC/Algorithms/Homeworks/Algorithm_Exam/03/src/total_orders.c" "/home/leonardo/DSSC/Algorithms/Homeworks/Algorithm_Exam/03/CMakeFiles/heap.dir/src/total_orders.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 03/CMakeFiles/heap.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/heap.dir/src/binheap.c.o"
+  "CMakeFiles/heap.dir/src/main.c.o"
+  "CMakeFiles/heap.dir/src/total_orders.c.o"
+  "heap"
+  "heap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/heap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
